959aa3d395ff5b806aad44627d9fed7139d7c157
zasdevs <zasdevs@gmail.com>
2022-08-15 16:18:44 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='youtube_dl_server',
-    version='0.4',
+    version='0.3',
     description='An API server based on yt-dlp',
     long_description='Get the videos from different sites using a server running yt-dlp',
     author='zasdevs',
