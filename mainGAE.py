64f70fc56fcbfd33de810a0dbf6335cbc7fd1023
zasdevs <zasdevs@gmail.com>
2022-08-15 17:17:54 +0500

    file restore

--- mainGAE.py
+++ mainGAE.py
@@ -3,10 +3,10 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), 'gae/lib'))
 
 # Monkeypatch some youtube_dl functions that use features not available in GAE
-import yt_dlp as ytdlp
+import yt_dlp
 
 # Modifying youtube_dl.utils.get_cachedir doesn't work
-ytdlp.extractor.youtube.get_cachedir = lambda *args, **kargs: None
+yt_dlp.extractor.youtube.get_cachedir = lambda *args, **kargs: None
 
 
 from youtube_dl_server.app import app  # noqa: app is used by GAE
